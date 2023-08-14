f893e11701b76b498ed709ba8de25cd81016bdc7
lucoiso <lucas.oliveiravb@gmail.com>
2023-08-14 17:46:06 +0100

    UE5.3 - Remove HoloLens

--- Source/HttpGPTImageModule/HttpGPTImageModule.Build.cs
+++ Source/HttpGPTImageModule/HttpGPTImageModule.Build.cs
@@ -11,11 +11,6 @@ public class HttpGPTImageModule : ModuleRules
         PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
         CppStandard = CppStandardVersion.Cpp17;
 
-        if (Target.Platform == UnrealTargetPlatform.HoloLens)
-        {
-            PrecompileForTargets = PrecompileTargetsType.Any;
-        }
-
         PublicDependencyModuleNames.AddRange(new[]
         {
             "Core",
