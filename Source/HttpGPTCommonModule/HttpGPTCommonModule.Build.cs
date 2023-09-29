dd80b9179998f00ed5c530957db81b76e87568fd
Lucas Vilas-Bôas <lucas.oliveiravb@gmail.com>
2023-09-29 18:51:53 +0100

    UE5.3 (#90)

--- Source/HttpGPTCommonModule/HttpGPTCommonModule.Build.cs
+++ Source/HttpGPTCommonModule/HttpGPTCommonModule.Build.cs
@@ -11,11 +11,6 @@ public class HttpGPTCommonModule : ModuleRules
         PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
         CppStandard = CppStandardVersion.Cpp17;
 
-        if (Target.Platform == UnrealTargetPlatform.HoloLens)
-        {
-            PrecompileForTargets = PrecompileTargetsType.Any;
-        }
-
         PublicDependencyModuleNames.AddRange(new[]
         {
             "Core",
